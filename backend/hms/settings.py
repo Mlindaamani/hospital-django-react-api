1e9e939314e6cb4f1f2c3ab8fbc47b3a38e8abd3
mlindaamani <mlindaadolf@gmail.com>
2025-04-18 22:33:16 +0300

    Removed unnecessary comments

--- backend/hms/settings.py
+++ backend/hms/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY= config('SECRET_KEY', cast=str)
 DEBUG = True
 
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://127.0.0.1:5173"]
+CORS_ALLOWED_ORIGINS = ["https://localhost"]
 
 
 REST_FRAMEWORK = {
